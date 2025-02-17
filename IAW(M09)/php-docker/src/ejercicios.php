0aed4c4e8881e886fc10298ab333ce5bbb2739b3
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-17 09:08:19 +0100

    2025-02-17 09:08:19

--- IAW(M09)/php-docker/src/ejercicios.php
+++ IAW(M09)/php-docker/src/ejercicios.php
@@ -1,7 +1,4 @@
-
-    <title>Exercicis curts d'estructures condicionals/title> 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<title>Exercicis curts d'estructures condicionals/title> 
 
               <?php
                
