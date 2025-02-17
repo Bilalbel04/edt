934ccb85f5dca57fcbe564cd870ffb58e33921d3
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-17 09:19:15 +0100

    2025-02-17 09:19:15

--- IAW(M09)/php-docker/src/ejercicios.php
+++ IAW(M09)/php-docker/src/ejercicios.php
@@ -10,9 +10,15 @@
     <h1>Ejercicios PHP</h1>
     <h4>Programa que comprova si una variable és major que zero o no. $n=-23 → dirà "-23 no és major que zero"</h4>
     <?php
-      for ($n = -23; $n <= 10; $n++) {
-       echo "<th>$n</th>";
-      }
+$n = -23;
+
+if ($n > 0) {
+    echo "$n és major que zero";
+} else {
+    echo "$n no és major que zero";
+}
+?>
+
     ?>
   </body>
 </html>
