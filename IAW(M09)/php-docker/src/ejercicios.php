ae7f97a48fa48b55c4430fd4abf0f8958b618195
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-17 09:33:27 +0100

    2025-02-17 09:33:27

--- IAW(M09)/php-docker/src/ejercicios.php
+++ IAW(M09)/php-docker/src/ejercicios.php
@@ -19,7 +19,7 @@
    ?>
    <h4>Programa semblant a l'anterior, però que dirà si la variable és positiva, negativa o zero</h4>
    <?php
-   $n = -0;
+   $n = 0;
    if ($n < -5 ) {
     echo "Esto es un numero nevatigo";
    }
