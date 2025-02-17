71618a439f5ec74067f0fc8d82d7530d0b097756
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-17 09:12:56 +0100

    2025-02-17 09:12:56

--- IAW(M09)/php-docker/src/ejercicios.php
+++ IAW(M09)/php-docker/src/ejercicios.php
@@ -9,5 +9,6 @@
   <body>
     <h1>Ejercicios PHP</h1>
     <h4>Programa que comprova si una variable és major que zero o no. $n=-23 → dirà "-23 no és major que zero"</h4>
+    <php>
   </body>
 </html>
