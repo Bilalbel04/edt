8f6ce84b62c10d573b6a7be599944c571dad30a1
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-17 09:39:09 +0100

    2025-02-17 09:39:09

--- IAW(M09)/php-docker/src/ejercicios.php
+++ IAW(M09)/php-docker/src/ejercicios.php
@@ -21,7 +21,7 @@
    <?php
    $n = -4;
    if ($n < 0 ) {
-    echo "<p>El numero $n es un numero negativo</p>";
+    echo <"El numero $n es un numero negativo</p>";
    }
    elseif ($n > 0 ){
      echo "Esto es un numero positivo";
