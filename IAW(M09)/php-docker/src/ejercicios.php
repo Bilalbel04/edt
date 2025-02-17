ed453716bbfe9e2fb72b45cbc6056ac9400e638d
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-17 09:09:58 +0100

    2025-02-17 09:09:58

--- IAW(M09)/php-docker/src/ejercicios.php
+++ IAW(M09)/php-docker/src/ejercicios.php
@@ -1,6 +1,6 @@
 <title>Exercicis curts d'estructures condicionals/title> 
 <?php
-                for ($a = 1; $a <= 10; $a++) {
-                    echo "$a";
-                  }
-              ?>
+    for ($a = 1; $a <= 10; $a++) {
+          echo "$a";
+    }
+?>
