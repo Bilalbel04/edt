3cd9c90bbb4dd8a9e77778d680f9994431b5f3bd
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-26 12:54:31 +0100

    2025-02-26 12:54:31

--- BDD/dockerpsql/sql/JOINS/Repventas.sql
+++ BDD/dockerpsql/sql/JOINS/Repventas.sql
@@ -68,9 +68,9 @@ SELECT nombre, ventas, ciudad
 r left join oficina o on r.ofinum = o.ofinum;
 
 /* 14. Obtenir una llista de la descripció dels productes per als 
-quals existeix alguna comanda en què se sol·licita una quantitat més 
-gran a les existències 
-del producte.*/
+quals existeix alguna comanda en què se sol·licita una quantitat més gran 
+a les existències del producte.*/
+
 
 
 /* 15. Llista els noms dels representants que tenen una quota superior a la del director.*/
