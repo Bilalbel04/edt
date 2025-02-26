9c381d6470d27628ec4762df65490cb3ec9847a1
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-26 12:40:32 +0100

    2025-02-26 12:40:32

--- BDD/dockerpsql/sql/JOINS/Repventas.sql
+++ BDD/dockerpsql/sql/JOINS/Repventas.sql
@@ -1,4 +1,4 @@
-/* A. Consultes simples
+/* A. Consultes simples*/
 /* 1. Obtenir les dades dels productes les existències dels quals estiguin entre 25 i 40 unitats. */
 
 /* 2. Obtenir els codis dels representants que han pres alguna comanda (evitant-ne la repetició). */
