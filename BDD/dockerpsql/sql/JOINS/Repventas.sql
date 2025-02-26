f1a87b305822a073af55709c4f08e6b74b675ee5
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-02-26 12:35:15 +0100

    2025-02-26 12:35:15

--- BDD/dockerpsql/sql/JOINS/Repventas.sql
+++ BDD/dockerpsql/sql/JOINS/Repventas.sql
@@ -20,49 +20,49 @@
 /* 10. Mostrar el nom i els dies que porta contractats els representants*/
 
 /* B. Consultes Multitaula*/
-/* Mostra dels representants el seu nom, la ciutat de la seva oficina, així com la seva regió. */
+/* 1. Mostra dels representants el seu nom, la ciutat de la seva oficina, així com la seva regió. */
 
-/* 1. Obtenir una llista de totes les comandes, mostrant el número de comanda, el seu import, el nom del client que el va fer i el límit de crèdit del client.*/
+/* 2. Obtenir una llista de totes les comandes, mostrant el número de comanda, el seu import, el nom del client que el va fer i el límit de crèdit del client.*/
 
-/* 2. Obtenir una llista de representants ordenada alfabèticament, en què es mostri el nom del representant, codi de la oficina on treballa, ciutat i la regió on ven.*/
+/* 3. Obtenir una llista de representants ordenada alfabèticament, en què es mostri el nom del representant, codi de la oficina on treballa, ciutat i la regió on ven.*/
 
-/* 3. Obtenir una llista de les oficines (ciutats, no codis) que tenen un objectiu superior a 360.000 euros. Per a cada oficina mostrar la ciutat, el seu objectiu, 
+/* 4. Obtenir una llista de les oficines (ciutats, no codis) que tenen un objectiu superior a 360.000 euros. Per a cada oficina mostrar la ciutat, el seu objectiu, 
 el nom del director i lloc del mateix.*/
 
-/* 4. Obtenir una llista de totes les comandes mostrant el seu número, el import i la descripció dels productes sol·licitats.*/
+/* 5. Obtenir una llista de totes les comandes mostrant el seu número, el import i la descripció dels productes sol·licitats.*/
 
-/* 5. Obtenir una llista de les comandes amb imports superiors a 4000. Mostrar el nom del client que va sol·licitar la comanda, número de la comanda, 
+/* 6. Obtenir una llista de les comandes amb imports superiors a 4000. Mostrar el nom del client que va sol·licitar la comanda, número de la comanda, 
 import de la mateixa, la descripció del producte sol·licitat i el nom del representant que el va prendre. Ordeneu la llista per client alfabèticament 
 i després per import de més gran a més petit.*/
 
-/* 6. Obtenir una llista de les comandes amb imports superiors a 2000 euros, mostrant el número de comanda, import, nom del client que ho 
+/* 7. Obtenir una llista de les comandes amb imports superiors a 2000 euros, mostrant el número de comanda, import, nom del client que ho 
 va sol·licitar i el nom del representant que va contactar amb el client per primera vegada.*/
 
-/* 7. Obtenir una llista de les comandes amb imports superiors a 150 euros, mostrant el codi de la comanda, l'import, el nom del client que ho va sol·licitar, 
+/* 8. Obtenir una llista de les comandes amb imports superiors a 150 euros, mostrant el codi de la comanda, l'import, el nom del client que ho va sol·licitar, 
 el nom del representant que va contactar amb ell per primera vegada i la ciutat de loficina on el representant treballa.*/
 
-/* 8. Llista les comandes preses durant el mes d'octubre de l'any 2003 , mostrant només el número de la comanda, el seu import, el nom del client que ho va realitzar,
+/* 9. Llista les comandes preses durant el mes d'octubre de l'any 2003 , mostrant només el número de la comanda, el seu import, el nom del client que ho va realitzar,
  la data i la descripció del producte sol·licitat*/
 
-/* 9. Obtenir una llista de totes les comandes preses per representants de oficines de la regió Est, mostrant només el número del comanda, la descripció del 
+/* 10. Obtenir una llista de totes les comandes preses per representants de oficines de la regió Est, mostrant només el número del comanda, la descripció del 
 producte i el nom del representant que ho va prendre*/
 
-/* 10. Obtenir les comandes preses els mateixos dies en què un nou representant va ser contractat. Mostrar número de comanda, import, data comanda.
+/* 11. Obtenir les comandes preses els mateixos dies en què un nou representant va ser contractat. Mostrar número de comanda, import, data comanda.
 
-/* 11. Obtenir una llista amb parelles de representants i oficines on la quota del representant és més gran o igual que l'objectiu de la oficina, 
+/* 12. Obtenir una llista amb parelles de representants i oficines on la quota del representant és més gran o igual que l'objectiu de la oficina, 
 sigui o no l'oficina on treballa. Mostra Nom del representant, quota del mateix, Ciutat de l'oficina, objectiu de la mateixa.*/
 
-/* 12. Mostra el nom, les vendes i la ciutat de l'oficina de cada representant de lempresa.
+/* 13. Mostra el nom, les vendes i la ciutat de l'oficina de cada 
+representant de lempresa.
 
-/* 13. Obtenir una llista de la descripció dels productes per als quals existeix alguna comanda en què se sol·licita una quantitat més gran a les existències 
+/* 14. Obtenir una llista de la descripció dels productes per als quals existeix alguna comanda en què se sol·licita una quantitat més gran a les existències 
 del producte.*/
 
-/* 14. Llista els noms dels representants que tenen una quota superior a la del director.
+/* 15. Llista els noms dels representants que tenen una quota superior a la del director.
 
-/* 15. Obtenir una llista dels representants que treballen en una oficina diferent de l'oficina on treballa el seu director, mostrant també el nom del director 
+/* 16. Obtenir una llista dels representants que treballen en una oficina diferent de l'oficina on treballa el seu director, mostrant també el nom del director 
 i el codi de l'oficina on treballa cadascun.*/
 
-/* 16. El mateix exercici anterior, però en comptes d'ofinum, la ciutat.*/
+/* 17. El mateix exercici anterior, però en comptes d'ofinum, la ciutat.*/
 
-/* 17. Mostrar el nom i el lloc de què són cap.*/
-
+/* 18. Mostrar el nom i el lloc de què són cap.*/
