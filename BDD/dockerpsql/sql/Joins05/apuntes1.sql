16c1e45f68d70492df0356c28813335ad4bba304
bilalbel1 <a241482bb@correu.escoladeltreball.org>
2025-03-05 12:29:42 +0100

    2025-03-05 12:29:42

--- BDD/dockerpsql/sql/Joins05/apuntes1.sql
+++ BDD/dockerpsql/sql/Joins05/apuntes1.sql
@@ -1,2 +1,9 @@
 Que es un join?
+Un join en SQL es una operación que combina registros de dos o más tablas en base a una condición relacionada entre ellas. 
+El resultado de un join es una tabla virtual que contiene columnas de ambas tablas, donde los registros coincidentes se combinan en una sola fila.
+
+Como hacer un join?
+SELECT columnas
+FROM tabla1
+JOIN tabla2 ON tabla1.columna_comun = tabla2.columna_comun;
 
